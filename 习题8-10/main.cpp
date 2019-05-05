0810bdacf18c20a2bcb2ad0251fdfbe397efcf23
zhangtingrui0211 <2358305634@qq.com>
2019-05-05 17:31:46 +0800

    作业

---
+++ 习题8-10/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+long zero(const long bigIntegers[],const size_t numberOfZero)
+{
+    return 0;
+}
+int add1AndSum(const int oneTooSmall[],const size_t number0fAdd1AndSum)
+{
+    int result=0;
+    for(size_t i=0;i<number0fAdd1AndSum;i++)
+        result=oneTooSmall[i];
+    return result;
+}
+
+int main()
+{
+    long x;
+    cout<<"The result is"<<zero(x)<<endl;
+}
